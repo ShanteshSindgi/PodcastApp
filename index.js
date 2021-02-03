4bde4275a5fddf382b8397dd99b9a780a8018730
Shantesh Sindgi <sindgishantesh@gmail.com>
2021-02-03 17:39:29 +0530

    Env Added

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const port = process.env.PORT;
+const dotenv = require("dotenv");
+dotenv.config();
 
-const express=require('express');
-const mongoose=require('mongoose')
-const app=express();
-const port=process.env.PORT || 3200;
 
-
-const bodyparser=require('body-parser');
+const bodyparser = require('body-parser');
 
 app.use(bodyparser.json());
 app.use(express.static(__dirname));
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
-  );
-  next();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
 });
-mongoose.connect('mongodb+srv://admin:admin@cluster0.s2prn.mongodb.net/Podcast?retryWrites=true&w=majority').then(()=>{
-  console.log('connected');
+
+// console.log(process.env.S)
+// console.log(process.env.MONGO_CONNECTION);
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+    console.log('connected');
 
 }).catch(
-  (err)=>{
-    console.log(err);
-  }
+    (err) => {
+        console.log(err);
+    }
 );
 
-app.listen(port,()=>{
-  console.log("running")
+app.listen(port, () => {
+    console.log("running")
 })
