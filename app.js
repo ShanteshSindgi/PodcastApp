eb71bd7a2f6f92d0f9fc6345ecdaff70e2c16d48
Shantesh Sindgi <sindgishantesh@gmail.com>
2021-02-03 22:28:27 +0530

    userRoute

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(express.static(__dirname));
+const router=require('./routes/users');
+const port = process.env.PORT;
+
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
+app.use('/users',router);
+app.use("/",(req,res)=>{
+    res.json("WELCOME TO SERVER")
+})
+
+// console.log(process.env.S)
+// console.log(process.env.MONGO_CONNECTION);
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+    console.log('connected');
+
+}).catch(
+    (err) => {
+        console.log(err);
+    }
+);
+console.log("port",process.env.PORT)
+app.listen(port, () => {
+    console.log("running")
+})
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+exports.loginUser=(req,res,next)=>{
+    res.json("Login")
+}
+
+exports.signUpUser=(req,res,next)=>{
+    res.json("Sign Up")
+}
